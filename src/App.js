df946c6a940601cf838aedaa717b1a1d36b0c0d0
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-14 17:22:25 -0500

    Add react-route-dom and basic SPA routes set

--- src/App.js
+++ src/App.js
@@ -3,13 +3,27 @@ import './App.css';
 import Header from './components/Header';
 import Cart from './pages/Cart';
 import Photos from './pages/Photos';
+import { Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
     <div>
       <Header />
       <h1>Home Page</h1>
+
+      <Switch>
+        <Route exact path="/">
+          <Photos />
+        </Route>
+        <Route exact path="/cart">
+          <Cart />
+        </Route>
+      </Switch>
     </div>
+
+
+
+
   );
 }
 
