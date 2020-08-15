23bb62ed4360416fbe5603d3ab08776a2f51913e
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-15 15:06:01 -0500

    Setting up Image component for Photos page

---
+++ src/components/Image.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+function Image({ className }) {
+    return (
+        <div className={`${className} image-container`}>
+            <img src={} className="image-grid" />
+        </div>
+    )
+}
+
+export default Image
