e69ca74e73b485bbad9f94c789d50d02219023e1
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-15 16:56:41 -0500

    Add hover state for image icons

--- src/components/Image.js
+++ src/components/Image.js
@@ -1,9 +1,21 @@
-import React from "react"
+import React, { useState } from "react";
 
 function Image({ imgObj, className }) {
+    const [hovered, setHovered] = useState(false);
+
     return (
-        <div className={`${className} image-container`}>
+        <div
+            onMouseEnter={() => setHovered(true)}
+            onMouseLeave={() => setHovered(false)}
+            className={`${className} image-container`}
+        >
             <img src={imgObj.url} className="image-grid" />
+            {hovered &&
+                <>
+                    <i className="ri-heart-line favorite"></i>
+                    <i className="ri-add-circle-line cart"></i>
+                </>
+            }
         </div>
     )
 }
