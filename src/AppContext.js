4017f7e31b56813cbb8b05d73cd263affbfdb4b0
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-15 14:59:58 -0500

    Fetch photo json inside of Context Provider

--- src/AppContext.js
+++ src/AppContext.js
@@ -1,9 +1,19 @@
-import React, { useState } from "react"
+import React, { useState, useEffect } from "react"
 
 const Context = React.createContext();
 
 function AppContextProvider({ children }) {
     const [photos, setPhotos] = useState([])
+
+    const url = 'https://raw.githubusercontent.com/bobziroll/scrimba-react-bootcamp-images/master/images.json'
+    useEffect(() => {
+        async function fetchData() {
+            const response = await fetch(url);
+            const data = await response.json();
+            setPhotos(data);
+        }
+        fetchData();
+    }, []) // Fetch on mount only
 
     return (
         <Context.Provider value={{ photos, setPhotos }}>
