cbf578c807e6e3fff314730ba245bcdedafe5e1d
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-16 13:57:01 -0500

    Update header cart icon logic for fill-empty contx

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,14 +1,25 @@
-import React from "react";
+import React, { useContext } from "react";
 import { Link } from "react-router-dom";
+import { AppContext } from "../AppContext";
 
 function Header() {
+    const { cartItems } = useContext(AppContext)
+
+    function cartIcon() {
+        if (cartItems.length > 0) {
+            return <i className="ri-shopping-cart-fill ri-fw ri-2x"></i>
+        } else if (cartItems.length === 0) {
+            return <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+        }
+    }
+
     return (
         <header>
             <Link to='/'>
                 <h2>Pic Some</h2>
             </Link>
             <Link to='/cart'>
-                <i className="ri-shopping-cart-line ri-fw ri-2x"></i>
+                {cartIcon()}
             </Link>
         </header>
     )
