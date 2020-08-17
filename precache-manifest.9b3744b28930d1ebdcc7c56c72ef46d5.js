d5976fd8e99230cb4e053d7f52530596ea368bc6
Justin Lowen <Justin.G.Lowen@Gmail.com>
2020-08-17 12:30:32 -0500

    Updates

---
+++ precache-manifest.9b3744b28930d1ebdcc7c56c72ef46d5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8c413ef32f372904b7e49b088a0c2723",
+    "url": "/picsomereact/index.html"
+  },
+  {
+    "revision": "de927151312968902906",
+    "url": "/picsomereact/static/css/main.6d28b708.chunk.css"
+  },
+  {
+    "revision": "f23391500bc1a595d593",
+    "url": "/picsomereact/static/js/2.a638863b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/picsomereact/static/js/2.a638863b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "de927151312968902906",
+    "url": "/picsomereact/static/js/main.6af630ac.chunk.js"
+  },
+  {
+    "revision": "f625d15823e7fbee2175",
+    "url": "/picsomereact/static/js/runtime-main.6759379b.js"
+  }
+]);
